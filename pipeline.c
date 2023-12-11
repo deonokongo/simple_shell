40ae188b6fc934d4e284f85053a8671a8220d391
kevin kirui <kipkoechkevin42@gmail.com>
2023-12-11 14:18:06 -0800

    removed some errors

--- pipeline.c
+++ pipeline.c
@@ -11,58 +11,58 @@ void run_pipeline(char *command)
 {
 	int pipe_fds[2];
 	while (*command != '\0')
-  {
-	  if (pipe(pipe_fds) == -1) 
-	  {
-		  perror("pipe");
-		  exit(EXIT_FAILURE);
-	  }
-	  pid_t child_pid = fork();
-	  
-	  if (child_pid == 0)
-	  {
-		  close(pipe_fds[0]);
-		  dup2(pipe_fds[1], STDOUT_FILENO);
-		  close(pipe_fds[1]);
-		  
-		  if (is_builtin(command))
-		  {
-			  char *args[MAX_ARGS];
-			  int arg_count = 0;
-			  char *token = strtok(command, " ");
-			  
-			  while (token != NULL && arg_count < MAX_ARGS - 1)
-			  {
-				  args[arg_count++] = token;
-				  token = strtok(NULL, " ");
-			  }
-			  args[arg_count] = NULL;
-			  execute_builtin(args);
-		  }
-		  else
-		  {
-			  execute_process(command, "");
-		  }
-		  exit(EXIT_SUCCESS);
-	  }
-	  else if (child_pid > 0)
-	  {
-		  close(pipe_fds[1]);
-		  waitpid(child_pid, NULL, 0);
-		  close(pipe_fds[0]);
-	  }
-	  else
-	  {
-		  perror("fork");
-		  exit(EXIT_FAILURE);
-	  }
-	  while (*command != '\0' && *command != '|')
-	  {
-		  command++;
-	  }
-	  if (*command == '|')
-	  {
-		  command++;
-	  }
-  }
+	{
+		if (pipe(pipe_fds) == -1)
+		{
+			perror("pipe");
+			exit(EXIT_FAILURE);
+		}
+		pid_t child_pid = fork();
+
+		if (child_pid == 0)
+		{
+			close(pipe_fds[0]);
+			dup2(pipe_fds[1], STDOUT_FILENO);
+			close(pipe_fds[1]);
+
+			if (is_builtin(command))
+			{
+				char *args[MAX_ARGS];
+				int arg_count = 0;
+				char *token = strtok(command, " ");
+
+				while (token != NULL && arg_count < MAX_ARGS - 1)
+				{
+					args[arg_count++] = token;
+					token = strtok(NULL, " ");
+				}
+				args[arg_count] = NULL;
+				execute_builtin(args);
+			}
+			else
+			{
+				execute_process(command, "");
+			}
+			exit(EXIT_SUCCESS);
+		}
+		else if (child_pid > 0)
+		{
+			close(pipe_fds[1]);
+			waitpid(child_pid, NULL, 0);
+			close(pipe_fds[0]);
+		}
+		else
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		while (*command != '\0' && *command != '|')
+		{
+			command++;
+		}
+		if (*command == '|')
+		{
+			command++;
+		}
+	}
 }
--- run_shell_from_file.c
+++ run_shell_from_file.c
@@ -11,24 +11,21 @@
  */
 int run_shell_from_file(char *filename)
 {
-  FILE *file = fopen(filename, "r");
-
-  if (!file)
-  {
-    print_kide("Error: Unable to open file '%s'\n", filename);
-    return 1;
-  }
-  char line[MAX_LINE_LENGTH];
-
-  while (fgets(line, MAX_LINE_LENGTH, file) != NULL)
-  {
-    line[strcspn(line, "\n")] = '\0';
-    
-    if (strlen(line) > 0)
-    {
-      system(line);
-    }
-  }
-  fclose(file);
-  return 0;
+	FILE *file = fopen(filename, "r");
+	if (!file)
+	{
+		print_kide("Error: Unable to open file '%s'\n", filename);
+		return (1);
+	}
+	char line[MAX_LINE_LENGTH];
+	while (fgets(line, MAX_LINE_LENGTH, file) != NULL)
+	{
+		line[strcspn(line, "\n")] = '\0';
+		if (strlen(line) > 0)
+		{
+			system(line);
+		}
+	}
+	fclose(file);
+	return (0);
 }
