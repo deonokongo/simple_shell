6ef1f825c4303418752d30ccb166d1c0e2788b96
kevin kirui <kipkoechkevin42@gmail.com>
2023-12-13 11:45:39 -0800

    debug

--- 0-prompt.c
+++ 0-prompt.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
  * prompt - displays the shell command prompt
  * Return: void
@@ -8,12 +7,11 @@ void prompt(void)
 {
 	print_kide("(simple_shell) $ ");
 	char *command = NULL;
-
 	size_t buffer = 0;
 
 	my_getline(&command, &buffer);
 	print_tokens(command);
-	execute_process(command, "");
+	execute_process(command, ""); 
 	free(command);
 }
 
