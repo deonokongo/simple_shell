071480919cecf6910aa77da22f152e036b9d9014
deonokongo <lilian.deon29@gmail.com>
2023-12-13 16:21:43 -0800

    removed .swp

--- 0-prompt.c
+++ 0-prompt.c
@@ -8,19 +8,16 @@ char *prompt(void)
 {
 	char *command = NULL;
 	size_t buffer = 0;
+	size_t length = strlen(command);
+	ssize_t characters_read = my_getline(&command, &buffer);
 
 	print_kide("(simple_shell)$ ");
-
-	ssize_t characters_read = my_getline(&command, &buffer);
 
 	if (characters_read == -1)
 	{
 		perror("my_getline");
 		exit(EXIT_FAILURE);
 	}
-
-	size_t length = strlen(command);
-
 	if (length > 0 && command[length - 1] == '\n')
 	{
 		command[length - 1] = '\0';
