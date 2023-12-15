2edd816602a2fa2a9024cba4e3499a6d860418c3
kevin kirui <kipkoechkevin42@gmail.com>
2023-12-15 09:39:54 -0800

    debug

--- environ.c
+++ environ.c
@@ -46,7 +46,7 @@ int _mysetenv(info_t *info)
 {
 	if (info->argc != 3)
 	{
-		_eputs("Incorrect number of arguements\n");
+		_puts("Incorrect number of arguements\n");
 		return (1);
 	}
 	if (_setenv(info, info->argv[1], info->argv[2]))
@@ -66,7 +66,7 @@ int _myunsetenv(info_t *info)
 
 	if (info->argc == 1)
 	{
-		_eputs("Too few arguements.\n");
+		_puts("Too few arguements.\n");
 		return (1);
 	}
 	for (i = 1; i <= info->argc; i++)
